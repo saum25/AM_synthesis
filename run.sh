d60146ed3b306a7abb0499b65ddf0b65cfb8b25b
Saumitra Mishra <saum.bits@gmail.com>
2019-02-24 19:55:20 +0000

    updating code to run AM for single neuron case with split sigmoid

--- run.sh
+++ run.sh
@@ -7,21 +7,30 @@ clear all
 
 # remove all the results
 rm -rf results
-rm -rf params_dump*
 
 # if needed run the code for SGD optimisation
 # 1 -> run for SGD as well
 # 0 -> only Adam
 
 sgd_optm=1
+act_min=1
 
 # create results directory
 mkdir results
-mkdir results/Adam
 
-if [ $sgd_optm -eq 1 ]
-then
+mkdir results/Adam
+mkdir results/Adam/maximise
+if [ $act_min -eq 1 ]; then
+	mkdir results/Adam/minimise
+fi
+
+
+if [ $sgd_optm -eq 1 ]; then
 	mkdir results/SGD
+	mkdir results/SGD/maximise
+	if [ $act_min -eq 1 ]; then
+		mkdir results/SGD/minimise
+	fi
 fi
 
 # string variables for naming the results directory
@@ -31,39 +40,59 @@ rp_str="_rp_"
 # watch out there is no space between '=' sign
 #scale_factor=0.1
 
-for neuron_idx in 0 1
+# variable to assist in writing header in the csv only for the first setting of hyper parameters
+setting_count=1
+
+# number of optimisation steps
+iter=5000
+
+echo --------- optimisation starts ---------------
+echo 
+
+for lr in 1.0 0.1 0.01 0.001 0.0001
 do
-	Adam_path=results/Adam/neuron$neuron_idx
-	mkdir $Adam_path
-	if [ $sgd_optm -eq 1 ]
-	then
-		SGD_path=results/SGD/neuron$neuron_idx
-		mkdir $SGD_path
-	fi
+	# bash does''t support floating point arithmetic, so use bc to process
+	# watch for the use of $ sign while saving the output to another variable
+	# currently not used
+	#adaptive_reg=$(echo $lr*$scale_factor | bc -l)
 
-	for lr in 1.0 0.1 0.01 0.001 0.0001
+	for rp in 1.0 0.1 0.01 0.001 0.0001
 	do
-		# bash does''t support floating point arithmetic, so use bc to process
-		# watch for the use of $ sign while saving the output to another variable
-		# currently not used
-		#adaptive_reg=$(echo $lr*$scale_factor | bc -l)
+		echo ++++++++++++++++++++ learning rate: $lr regularisation paramter: $rp hyperparam setting count: $setting_count ++++++++++++++
 
-		for rp in 1.0 0.1 0.01 0.001 0.0001
-		do
-			echo ++++++++++++++++++++learning rate: $lr regularisation paramter: $rp++++++++++++++
+		echo 
+		echo +++++++ Adam - activation maximisation case ++++++++
+		mkdir results/Adam/maximise/$lr_str$lr$rp_str$rp
+		mkdir results/Adam/maximise/$lr_str$lr$rp_str$rp/examples
+		python activation_maximisation.py --output_dir results/Adam/maximise --init_lr $lr --reg_param $rp --n_iters $iter --stats_csv 'results/optm_stats_Adam.csv' --count $setting_count
 
-			mkdir results/Adam/neuron$neuron_idx/$lr_str$lr$rp_str$rp
-			mkdir results/Adam/neuron$neuron_idx/$lr_str$lr$rp_str$rp/examples
-			python activation_maximisation.py --output_dir $Adam_path --init_lr $lr --reg_param $rp --n_iters 1000 --param_file 'params_dump_Adam.txt' --neuron $neuron_idx --n_out_neuron 2 --layer 'fc9'
 
-			if [ $sgd_optm -eq 1 ]
-			then
-				mkdir results/SGD/neuron$neuron_idx/$lr_str$lr$rp_str$rp
-				mkdir results/SGD/neuron$neuron_idx/$lr_str$lr$rp_str$rp/examples
-				python activation_maximisation.py --output_dir $SGD_path --optimizer 'SGD' --init_lr $lr --reg_param $rp --n_iters 1000 --param_file 'params_dump_SGD.txt' --neuron $neuron_idx --n_out_neuron 2 --layer 'fc9'
-			fi
+		if [ $act_min -eq 1 ];  then
+			echo +++++++ Adam - activation minimisation case ++++++++
+			mkdir results/Adam/minimise/$lr_str$lr$rp_str$rp
+			mkdir results/Adam/minimise/$lr_str$lr$rp_str$rp/examples
+			python activation_maximisation.py --output_dir results/Adam/minimise --init_lr $lr --reg_param $rp --n_iters $iter --stats_csv 'results/optm_stats_Adam.csv' --count $setting_count --minimise 
+		fi
 
-		done
+
+		if [ $sgd_optm -eq 1 ]; then
+			echo +++++++ SGD - activation maximisation case ++++++++
+			mkdir results/SGD/maximise/$lr_str$lr$rp_str$rp
+			mkdir results/SGD/maximise/$lr_str$lr$rp_str$rp/examples
+			python activation_maximisation.py --output_dir results/SGD/maximise --optimizer 'SGD' --init_lr $lr --reg_param $rp --n_iters $iter --stats_csv 'results/optm_stats_SGD.csv' --count $setting_count
+		fi
+
+		if [[ $sgd_optm -eq 1 && $act_min -eq 1 ]]; then
+			echo +++++++ SGD - activation minimisation case ++++++++
+			mkdir results/SGD/minimise/$lr_str$lr$rp_str$rp
+			mkdir results/SGD/minimise/$lr_str$lr$rp_str$rp/examples
+			python activation_maximisation.py --output_dir results/SGD/minimise --optimizer 'SGD' --init_lr $lr --reg_param $rp --n_iters $iter --stats_csv 'results/optm_stats_SGD.csv' --count $setting_count --minimise
+		fi
+
+		setting_count=$((setting_count+1))
 
 	done
+
 done
+
+echo ----------optimisation ends -------------------
